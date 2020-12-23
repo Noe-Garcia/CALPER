7c515fdc69b558c5d7effef4288cfb9b3ad59bc9
noelbejarano <69813379+noelbejarano@users.noreply.github.com>
2020-12-22 22:22:56 -0600

    Extracción de datos del API BANXICO
    Se logró la conexión exitosa con el SIE API del Banco de México y se extrageron los datos necesarios para el analisis con algoritmos.

--- backend/python-restapi/src/app.py
+++ backend/python-restapi/src/app.py
@@ -1,5 +1,7 @@
-#SP30577 INPC - Inflacion Mensual
+#SP30578 INPC - Inflacion Anual
 #SP68257 Valor de UDIS en MXN
+#SR16773 Inflación general en los proximos 12 meses
+#SR14755 Tasa de Interes del Cete
 
 from flask import Flask, jsonify
 from users import users
@@ -18,19 +20,40 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
     #app.run(host="0.0.0.0", port=8080, debug=True)
-    url = 'https://www.banxico.org.mx/SieAPIRest/service/v1/series/SP30577,SP68257/datos/oportuno?token=2ac7879679a24f782b0ab50a26563302baaad76fc53c52968a1a113e7e6aae5a'
+
+    #Aqui consumimos el API de BANXICO
+    url = 'https://www.banxico.org.mx/SieAPIRest/service/v1/series/SP30578,SP68257,SR16773,SR14755/datos/oportuno?token=2ac7879679a24f782b0ab50a26563302baaad76fc53c52968a1a113e7e6aae5a'
     response = requests.get(url)
     
-    if response.status_code == 200:
-        response_json = response.json()
+    if response.status_code == 200: #Funciones para algoritmos de consulta
+        response_json = json.loads(response.content) #Consulta JSON con series de BANXICO
         bmx = response_json['bmx']
         series = bmx['series']
-        serieinf = series[0]
-        datos = serieinf['datos']
-        indice = datos[-1]
-        dato = indice['dato']
-        print(dato) #Falta encontrar algoritmo de extraccion exacta del dato
+
+        for inpclist in series:
+            if inpclist['idSerie'] == 'SP30578':
+                for inpcdata in inpclist['datos']:
+                    inpc = inpcdata['dato']
+                    inpcdate = inpcdata['fecha']
+                    print(inpc , inpcdate)
+
+        for udislist in series:
+            if udislist['idSerie'] == 'SP68257':
+                for udisdata in udislist['datos']:
+                    udis = udisdata['dato']
+                    udisdate = udisdata['fecha']
+                    print(udis , udisdate)
+
+        for infglist in series:
+            if infglist['idSerie'] == 'SR16773':
+                for infgdata in infglist['datos']:
+                    infg = infgdata['dato']
+                    infgdate = infgdata['fecha']
+                    print(infg , infgdate)
         
-        # response_json = json.loads(response.text)
-        # dato = response_json['']
-        # print(dato)
+        for cetelist in series:
+            if cetelist['idSerie'] == 'SR14755':
+                for cetedata in cetelist['datos']:
+                    cete = cetedata['dato']
+                    cetedate = cetedata['fecha']
+                    print(cete , cetedate)
