2dbe1903052f355c6dcefb3f0f0532f92962c4ee
Noe-Garcia <noe.garcia@fintecheando.mx>
2020-12-23 23:20:08 -0600

    Revert "Mostrando resultados"
    This reverts commit 57552f09ef4f753b9d800130d3419cc40fc0ce12.

--- frontend/calper/src/app/calculador/calculador.page.ts
+++ frontend/calper/src/app/calculador/calculador.page.ts
@@ -1,42 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { Key } from 'protractor';
 import { CalperApiService } from '../calper-api.service'
-<<<<<<< Updated upstream
 type jsonResponse = {
   message: string;
   status: string;
-=======
-type Response={
-  ahorros: string;
-  inflacionPronosticada:string;
-  valorInvirtiendoEnCetes:string;
-  valorReal:string;
->>>>>>> Stashed changes
 }
 @Component({
   selector: 'app-calculador',
   templateUrl: './calculador.page.html',
   styleUrls: ['./calculador.page.scss'],
 })
+
 export class CalculadorPage implements OnInit {
-<<<<<<< Updated upstream
   message: string;
   status: string;
 
   constructor(private calperApi: CalperApiService) { }
 
-=======
-  ahorros:number;
-  response: any;
-  responses:any=[];
-  constructor(private calperApi: CalperApiService){}
->>>>>>> Stashed changes
   ngOnInit() {
+
   }
   calcular() {
     // console.log("Funciona!")
     this.calperApi.getConsulta()
-<<<<<<< Updated upstream
       .subscribe(
         res => this.extract(res.toString),
         err => console.log(err)
@@ -47,16 +33,4 @@ export class CalculadorPage implements OnInit {
     this.message = newObj;
     console.log(this.message);
   }
-=======
-    .subscribe(
-      res => this.saveRes(res),
-      err => console.log(err)
-    )
-  }
-  saveRes(res){
-    res = this.response;
-    console.log(res);
-    this.responses = res;
-  }
->>>>>>> Stashed changes
 }
